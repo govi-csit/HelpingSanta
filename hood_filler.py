53999de0d5fb67c2d303465e4ec1582a264abb6e
govi-csit <govi.csit@gmail.com>
2021-10-10 20:43:09 +0530

    adding code

---
+++ hood_filler.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 10 18:59:57 2021
+
+@author: Govi
+"""
+
+
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+#the function compute minimum number of weights to fill hood capacity
+def minimum_weigths_compute(present_weights, hood_capacity):
+    
+    '''
+    Parameters
+    ----------
+    S : list
+        list of weights
+   
+    n : integer
+        available capacity
+
+    Returns
+    -------
+    items : list
+            a list of present weights to prepare the hood minimizing the number of items
+    '''
+    
+    
+    #total number of weight
+    total_weights = len(present_weights)
+    
+    #creating table to store intermediate results
+    table = [[0 for i in range(hood_capacity + 1)]
+                for i in range(total_weights + 1)]
+    
+    #initializing the array to maximum values
+    for i in range(1, hood_capacity + 1):
+        table[0][i] = 10**9 - 1
+    
+    #iterating over weights
+    for i in range(1, total_weights + 1):
+        
+        #couning minimum number of weights to represent 'j' using already seen weights
+        for j in range(1, hood_capacity + 1):
+            
+            if (present_weights[i - 1] > j):
+                table[i][j] = table[i - 1][j]
+            else:
+                #considerint minimum of {previous sequence weights count, new sequence weights count} of j
+                table[i][j] = min(table[i - 1][j],
+                                table[i][j - present_weights[i - 1]] + 1)
+        
+    
+    row_index = total_weights
+        
+    #stores computed weight sequence
+    items = []
+    
+    #iterate over table to find the weights
+    while row_index>=1:
+        if table[row_index-1][hood_capacity] > table[row_index][hood_capacity]:
+            items.append(present_weights[row_index-1])
+            hood_capacity = hood_capacity - present_weights[row_index-1]
+        else:
+            row_index = row_index - 1
+   
+    
+    return items
+
+
+
+@app.route('/hoodfiller', methods=['GET', 'POST'])
+def compute_weigths():
+
+    content = request.json
+    hood_capacity = content['hood_capacity']
+    present_weights = content['present_weights']
+    
+    print('hood_capacity : ', hood_capacity)
+    print('present_weights : ', hood_capacity)
+    
+    #calling the core logic function for computing the weights
+    items = minimum_weigths_compute(present_weights, hood_capacity)
+    
+    print('solution : ', items)    
+    
+    return jsonify({"present_weights" : items})
+    
+    
+
+if __name__ == '__main__':
+    app.run(debug = False)
+    
